158da687e102e79798879a67b531cb3ab1e95f4b
AlexeyZh <waltafunk@gmail.com>
2023-03-12 20:52:52 +0300

    add test_unlogin(from test_login -> test_logout)

---
+++ saucedemo/tests/login/test_logout.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page, expect
+
+
+def test_unlogin(page: Page) -> None:
+    page.goto("https://www.saucedemo.com/")
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.get_by_text("Login").click()
+    burger_menu = page.locator("//*[@id='react-burger-menu-btn']")
+    burger_menu.click()
+    logout = page.locator("//*[@id='logout_sidebar_link']")
+    logout.click()
+    login_logo = page.locator("//div[@class='login_logo']")
+    expect(login_logo).to_have_text('Swag Labs')
+    user_name = page.locator("//*[@id='user-name']")
+    expect(user_name).to_have_text('')
