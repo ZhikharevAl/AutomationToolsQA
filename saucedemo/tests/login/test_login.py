389fa94988b11e4a66c5c4582c7bcb7ae5a1f421
AlexeyZh <waltafunk@gmail.com>
2023-03-12 20:24:22 +0300

    add test_login_no_password

--- saucedemo/tests/login/test_login.py
+++ saucedemo/tests/login/test_login.py
@@ -32,3 +32,14 @@ def test_login_no_user_name(page: Page) -> None:
     expected_fail_massage = 'Username is required'
     error_msg_locator = page.locator("//h3")
     expect(error_msg_locator).to_contain_text(expected_fail_massage)
+
+
+def test_login_no_password(page: Page) -> None:
+    page.goto("https://www.saucedemo.com/")
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("")
+    page.get_by_text("Login").click()
+
+    expected_fail_massage = 'Password is required'
+    error_msg_locator = page.locator("//h3")
+    expect(error_msg_locator).to_contain_text(expected_fail_massage)
