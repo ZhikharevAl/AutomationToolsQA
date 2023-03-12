4c6a38ea5dacb1d885ccb68b560e007fd1fbad70
AlexeyZh <waltafunk@gmail.com>
2023-03-12 20:02:14 +0300

    add test_login_with_invalid_user

---
+++ saucedemo/tests/login/test_login.py
@@ -0,0 +1,23 @@
+from playwright.sync_api import Page, expect
+
+
+def test_login_with_standard_user(page: Page) -> None:
+    page.goto("https://www.saucedemo.com/")
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.get_by_text("Login").click()
+
+    products_header = page.locator("//span[@class='title']")
+
+    expect(products_header).to_have_text('Products')
+
+
+def test_login_with_invalid_user(page: Page) -> None:
+    page.goto("https://www.saucedemo.com/")
+    page.get_by_placeholder("Username").fill("invalid_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.get_by_text("Login").click()
+
+    expected_fail_massage = 'Username and password do not match any user in this service'
+    error_msg_locator = page.locator("//h3")
+    expect(error_msg_locator).to_contain_text(expected_fail_massage)
