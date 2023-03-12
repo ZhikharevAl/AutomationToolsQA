3cdd5cf9794ff0b89a0ad28af1e7380f87307b1e
AlexeyZh <waltafunk@gmail.com>
2023-03-12 20:46:05 +0300

    add test_unlogin

--- saucedemo/tests/login/test_login.py
+++ saucedemo/tests/login/test_login.py
@@ -12,6 +12,21 @@ def test_login_with_standard_user(page: Page) -> None:
     expect(products_header).to_have_text('Products')
 
 
+def test_unlogin(page: Page) -> None:
+    page.goto("https://www.saucedemo.com/")
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.get_by_text("Login").click()
+    burger_menu = page.locator("//*[@id='react-burger-menu-btn']")
+    burger_menu.click()
+    logout = page.locator("//*[@id='logout_sidebar_link']")
+    logout.click()
+    login_logo = page.locator("//div[@class='login_logo']")
+    expect(login_logo).to_have_text('Swag Labs')
+    user_name = page.locator("//*[@id='user-name']")
+    expect(user_name).to_have_text('')
+
+
 def test_login_with_invalid_user(page: Page) -> None:
     page.goto("https://www.saucedemo.com/")
     page.get_by_placeholder("Username").fill("invalid_user")
