5bd01e805316de28da8cb63aaf68d341ce2c0cde
AlexeyZh <waltafunk@gmail.com>
2023-03-20 23:48:04 +0300

    add est_checkout_your_inf

--- saucedemo/src/pages/CheckoutYourInformationPage.py
+++ saucedemo/src/pages/CheckoutYourInformationPage.py
@@ -2,6 +2,31 @@ class CheckoutYourInformationPage:
     def __init__(self, page):
         self.page = page
         self._checkout_header = page.locator("span[class='title']")
+        self._first_name = page.locator("input[id='first-name']")
+        self._last_name = page.locator("input[id='last-name']")
+        self._zipcode = page.locator("input[id='postal-code']")
 
     def checkout_header(self):
         return self._checkout_header
+
+    def first_name_locator(self):
+        return self._first_name
+
+    def add_first_name(self):
+        self.first_name_locator().fill("John")
+        return self
+
+    def last_name_locator(self):
+        return self._last_name
+
+    def add_last_name(self):
+        self.last_name_locator().fill("Snow")
+        return self
+
+    def zipcode_locator(self):
+        return self._zipcode
+
+    def add_zipcode(self):
+        self.zipcode_locator().fill("458769")
+        return self
+
---
+++ saucedemo/tests/checkout/test_checkout_your_inf.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import expect
+
+
+from saucedemo.src.pages.CartPage import CartPage
+from saucedemo.src.pages.CheckoutYourInformationPage import CheckoutYourInformationPage
+from saucedemo.src.pages.LoginPage import LoginPage
+
+
+def test_checkout_your_inf(setup_teardown) -> None:
+    page = setup_teardown
+    credentials = {'username': 'standard_user', 'password': 'secret_sauce'}
+    login_p = LoginPage(page)
+    products_p = login_p.do_login(credentials)
+    products_p.click_add_to_cart()
+    products_p.click_cart()
+    cart_p = CartPage(page)
+    cart_p.click_checkout()
+    checkout_p = CheckoutYourInformationPage(page)
+
+    checkout_p.add_first_name()
+    checkout_p.add_last_name()
+    checkout_p.add_zipcode()
+
+
+    expect()
