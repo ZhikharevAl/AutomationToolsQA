b876022fcc732a79d9ab6805f24efbb9992037af
AlexeyZh <waltafunk@gmail.com>
2023-03-21 00:03:43 +0300

    add CheckoutOverviewPage

---
+++ saucedemo/src/pages/CheckoutOverview/ChekoutOverviewPage.py
@@ -0,0 +1,11 @@
+
+class CheckoutOverviewPage:
+
+    def __init__(self, page):
+        self.page = page
+        self._checkout_overview = page.locator("//*[text() = 'Checkout: Overview']")
+
+    def checkout_overview(self):
+        return self._checkout_overview
+
+
