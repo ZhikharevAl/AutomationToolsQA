6acf791f4c22c976c96ed846e4acdc1b25a55bb1
AlexeyZh <waltafunk@gmail.com>
2023-03-21 23:32:27 +0300

    add test_finish

--- saucedemo/src/pages/CheckoutOverviewPage.py
+++ saucedemo/src/pages/CheckoutOverviewPage.py
@@ -1,9 +1,16 @@
-
 class CheckoutOverviewPage:
 
     def __init__(self, page):
         self.page = page
         self._checkout_overview = page.locator("//*[text() = 'Checkout: Overview']")
+        self._finish = page.locator("button[id='finish']")
 
     def checkout_overview(self):
         return self._checkout_overview
+
+    def finish_locator(self):
+        return self._finish
+
+    def click_finish(self):
+        self.finish_locator().click()
+        return self
---
+++ saucedemo/tests/checkout_overview/test_finish.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import expect
+
+from saucedemo.src.pages.CartPage import CartPage
+from saucedemo.src.pages.CheckoutOverviewPage import CheckoutOverviewPage
+from saucedemo.src.pages.CheckoutYourInformationPage import CheckoutYourInformationPage
+from saucedemo.src.pages.CheckoutCompletePage import CheckoutCompletePage
+from saucedemo.src.pages.LoginPage import LoginPage
+
+
+def test_finish(setup_teardown) -> None:
+    page = setup_teardown
+    credentials = {'username': 'standard_user', 'password': 'secret_sauce'}
+    login_p = LoginPage(page)
+    products_p = login_p.do_login(credentials)
+    products_p.click_add_to_cart()
+    products_p.click_cart()
+    cart_p = CartPage(page)
+    cart_p.click_checkout()
+    checkout_p = CheckoutYourInformationPage(page)
+
+    checkout_p.add_first_name()
+    checkout_p.add_last_name()
+    checkout_p.add_zipcode()
+
+    checkout_p.click_continue()
+
+    checkout_o = CheckoutOverviewPage(page)
+    checkout_c = CheckoutCompletePage(page)
+    checkout_o.click_finish()
+
+    expect(checkout_c.checkout_complete_title()).to_have_text("Checkout: Complete!")
