e2e667742fae3b0a3ac6a88e0aa68fb8938eaf47
AlexeyZh <waltafunk@gmail.com>
2023-04-30 23:12:19 +0300

    added logging

--- saucedemo/tests/conftest.py
+++ saucedemo/tests/conftest.py
@@ -1,3 +1,4 @@
+import logging
 import pytest
 
 
@@ -6,3 +7,15 @@ def setup_teardown(page) -> None:
     page.set_viewport_size({'width': 1536, 'height': 800})
     page.goto("https://www.saucedemo.com")
     yield page
+
+
+def pytest_configure():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def pytest_runtest_logreport(report):
+    if report.when == "setup" and logging.getLogger().isEnabledFor(logging.INFO):
+        logging.info("Test run: %s", report.nodeid)
+    elif report.when == "teardown" and logging.getLogger().isEnabledFor(logging.INFO):
+        logging.info("Test completion: %s", report.nodeid)
+
